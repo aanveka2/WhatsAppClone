3b99cfb9adb16c66baa8995e73d63f1256795c39
Akshay Kumar Anvekar <akshayanvekar@Akshays-MacBook-Pro.local>
2021-05-06 01:46:18 -0400

    Change in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {auth, db} from '../firebase';
 import Login from "./login";
-import Loading from "../components/loading";
+import Loading from "../components/Loading";
 import { useEffect } from 'react';
 import firebase from "firebase";
 function MyApp({ Component, pageProps }) {
